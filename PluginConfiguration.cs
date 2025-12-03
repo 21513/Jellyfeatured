f5120b1a0a5cc42ce7def2ce33e1c28f8de87ed4
baeac <baeacbusiness@gmail.com>
2025-12-03 20:38:58 +0100

    Cleaned up the config page

--- PluginConfiguration.cs
+++ PluginConfiguration.cs
@@ -8,42 +8,8 @@ namespace Jellyfeatured;
 public class PluginConfiguration : BasePluginConfiguration
 {
     /// <summary>
-    /// Gets or sets a value indicating whether the carousel auto-slide is enabled.
-    /// </summary>
-    public bool EnableAutoSlide { get; set; } = true;
-    
-    /// <summary>
-    /// Gets or sets the auto-slide interval in seconds.
-    /// </summary>
-    public int AutoSlideInterval { get; set; } = 6;
-    
-    /// <summary>
-    /// Gets or sets the number of recommendations per category.
-    /// </summary>
-    public int RecommendationsPerCategory { get; set; } = 1;
-    
-    /// <summary>
-    /// Gets or sets a value indicating whether to show movie recommendations.
-    /// </summary>
-    public bool ShowMovieRecommendations { get; set; } = true;
-    
-    /// <summary>
-    /// Gets or sets a value indicating whether to show series recommendations.
-    /// </summary>
-    public bool ShowSeriesRecommendations { get; set; } = true;
-    
-    /// <summary>
-    /// Gets or sets a value indicating whether to enable the hover effect.
-    /// </summary>
-    public bool EnableHoverEffect { get; set; } = true;
-    
-    /// <summary>
-    /// Gets or sets a value indicating whether to show ratings in the carousel.
-    /// </summary>
-    public bool ShowRatings { get; set; } = true;
-    
-    /// <summary>
     /// Gets or sets the title for the admin's pick (6th category).
+    /// When set, this content will be featured as an additional recommendation.
     /// </summary>
     public string AdminPickTitle { get; set; } = "";
 }
