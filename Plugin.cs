af158f9007b3a4a072b3fe6606725798a560ffed
baeac <baeacbusiness@gmail.com>
2025-12-03 16:54:09 +0100

    Updated web injection

--- Plugin.cs
+++ Plugin.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using MediaBrowser.Common.Configuration;
 using MediaBrowser.Common.Plugins;
 using MediaBrowser.Model.Serialization;
+using Microsoft.Extensions.Logging;
 
 namespace Jellyfeatured;
 
@@ -11,14 +12,19 @@ namespace Jellyfeatured;
 /// </summary>
 public class Plugin : BasePlugin<PluginConfiguration>
 {
+    private readonly ILogger<Plugin> _logger;
+
     /// <summary>
     /// Initializes a new instance of the <see cref="Plugin"/> class.
     /// </summary>
     /// <param name="applicationPaths">Instance of the <see cref="IApplicationPaths"/> interface.</param>
     /// <param name="xmlSerializer">Instance of the <see cref="IXmlSerializer"/> interface.</param>
-    public Plugin(IApplicationPaths applicationPaths, IXmlSerializer xmlSerializer)
+    /// <param name="logger">Instance of the <see cref="ILogger{Plugin}"/> interface.</param>
+    public Plugin(IApplicationPaths applicationPaths, IXmlSerializer xmlSerializer, ILogger<Plugin> logger)
         : base(applicationPaths, xmlSerializer)
     {
+        _logger = logger;
+        _logger.LogInformation("🎬 Jellyfeatured Plugin initialized successfully!");
     }
 
     /// <inheritdoc />
